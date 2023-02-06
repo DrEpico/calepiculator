f3e163100610724c7aae31b3a69338aaac42b738
ali <adadashipour@gmail.com>
2023-02-06 18:25:36 +0000

    Added functions to store numerical variable and the operator

--- script.js
+++ script.js
@@ -14,11 +14,30 @@ const div = function(numbers = []) {
     return numbers.reduce((a, b) => a / b); 
 };
 
-const power = function(numbers = []) {
-    if (numbers.length < 2) {
-        return 0;
-    }
-    return numbers[0] ** numbers[1];
-};
+// const power = function(numbers = []) {
+//     if (numbers.length < 2) {
+//         return 0;
+//     }
+//     return numbers[0] ** numbers[1];
+// };
 
 
+
+// function getValue() {
+//     let input = document.querySelector('#input').value;
+//     console.log(input);
+// }
+
+function storeVar(numbers = []){
+    numbers[0] = parseFloat(document.querySelector('#input').value);
+    console.log(numbers[0]);
+}
+const buttons = document.querySelectorAll("#container button");
+buttons.forEach(button => {
+    button.addEventListener("click", function() {
+        operator = this.innerHTML;
+        console.log(operator);
+    });
+});
+
+//if or switch operator statement and function call
