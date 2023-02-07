8b5b2462fac0257784455cb2c89c1aa0b5262b2a
ali <adadashipour@gmail.com>
2023-02-07 14:40:11 +0000

    Added... or more like separeted the eventListener function that takes the user input and logs in array and the eventListener function that listens to the 'equal to button' to end the calculation.

--- script.js
+++ script.js
@@ -1,7 +1,37 @@
 numbers = []
+const inputField = document.querySelector("form #input");
+const buttons = document.querySelectorAll("#container .midCalc");
+buttons.forEach(button => {
+    button.addEventListener("click", function() {
+        operator = this.innerHTML;
+        numbers[1] = numbers[0];
+        numbers[0] = parseFloat(document.querySelector('#input').value);
+        inputField.value = "";
+        console.log(numbers[0]); 
+        console.log(numbers[1]); 
+        console.log(operator); 
+        console.log(numbers);
+        updateNumbers = numbers;
+    });
+});
 
+const finalCalculation = document.querySelectorAll("#container .endCalc");
+finalCalculation.forEach(button => {
+    button.addEventListener("click", function() {  
+        numbers[1] = numbers[0];
+        numbers[0] = parseFloat(document.querySelector('#input').value);  
+        if(operator === "+"){
+            return console.log(sum(updateNumbers));
+        } else {
+            console.log("error")
+        }
+    });
+});
 const sum = function(numbers) {
-    return numbers.reduce((a, b) => a + b, 0);
+    return numbers.reduce(function(a, b){
+        console.log(a + b)
+        return a + b;
+    });
 };
 
 const sub = function(numbers) {
@@ -23,20 +53,5 @@ const div = function(numbers) {
 //     return numbers[0] ** numbers[1];
 // };
 
-const inputField = document.querySelector("form #input")
-const buttons = document.querySelectorAll("#container button");
-buttons.forEach(button => {
-    button.addEventListener("click", function() {
-        operator = this.innerHTML;
-        numbers[0] = parseFloat(document.querySelector('#input').value);
-        numbers[1] = numbers[0];
-        inputField.value = "";
-        
-        console.log(numbers[0]);
-        console.log(numbers[1]);
-        console.log(operator);
-        console.log(numbers);
-        //if or switch operator statement and do sum(numbers) and etc.
-    });
-});
+
 
