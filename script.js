51cc1ae063e434b2386129c5588220142c802266
ali <adadashipour@gmail.com>
2023-02-07 13:04:39 +0000

    Removed the onclick functions and updates around the same function.

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
-const sum = function(numbers = []) {
+numbers = []
+
+const sum = function(numbers) {
     return numbers.reduce((a, b) => a + b, 0);
 };
 
-const sub = function(numbers = []) {
+const sub = function(numbers) {
     return numbers.reduce((a, b) => a - b);
 };
 
-const multiply = function(numbers = []) {
+const multiply = function(numbers) {
     return numbers.reduce((a, b) => a * b, 1);
 };
 
-const div = function(numbers = []) {
+const div = function(numbers) {
     return numbers.reduce((a, b) => a / b); 
 };
 
@@ -21,23 +23,20 @@ const div = function(numbers = []) {
 //     return numbers[0] ** numbers[1];
 // };
 
-
-
-// function getValue() {
-//     let input = document.querySelector('#input').value;
-//     console.log(input);
-// }
-
-function storeVar(numbers = []){
-    numbers[0] = parseFloat(document.querySelector('#input').value);
-    console.log(numbers[0]);
-}
+const inputField = document.querySelector("form #input")
 const buttons = document.querySelectorAll("#container button");
 buttons.forEach(button => {
     button.addEventListener("click", function() {
         operator = this.innerHTML;
+        numbers[0] = parseFloat(document.querySelector('#input').value);
+        numbers[1] = numbers[0];
+        inputField.value = "";
+        
+        console.log(numbers[0]);
+        console.log(numbers[1]);
         console.log(operator);
+        console.log(numbers);
+        //if or switch operator statement and do sum(numbers) and etc.
     });
 });
 
-//if or switch operator statement and function call
