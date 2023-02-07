d414397f523c27d065fdd0cd4cc9391ca30e9278
ali <adadashipour@gmail.com>
2023-02-07 18:27:26 +0000

    The basic calculation works perfectly now

--- script.js
+++ script.js
@@ -10,7 +10,6 @@ buttons.forEach(button => {
         console.log(numbers[0]); 
         console.log(numbers[1]); 
         console.log(operator); 
-        console.log(numbers);
         updateNumbers = numbers;
     });
 });
@@ -19,50 +18,74 @@ const finalCalculation = document.querySelectorAll("#container .endCalc");
 finalCalculation.forEach(button => {
     button.addEventListener("click", function() {  
         numbers[1] = numbers[0];
-        numbers[0] = parseFloat(document.querySelector('#input').value);  
-        if(operator === "+"){
-            inputField.value = sum(updateNumbers);
-            //console.log(sum(updateNumbers));
-        } else {
-            console.log("error")
+        numbers[0] = parseFloat(document.querySelector('#input').value);
+        switch(true){
+            case operator === "+":
+                inputField.value = sum(updateNumbers);
+                console.log(updateNumbers);
+                console.log(sum(updateNumbers));
+                break;
+            case operator === "-":
+                inputField.value = sub(updateNumbers);
+                console.log(updateNumbers);
+                console.log(sub(updateNumbers));
+                break;
+            case operator === "x":
+                inputField.value = multiply(updateNumbers);
+                console.log(updateNumbers);
+                console.log(multiply(updateNumbers));
+                break;
+            case operator === "➗":
+                inputField.value = div(updateNumbers);
+                console.log(updateNumbers);
+                console.log(div(updateNumbers));
+                break;
+            case operator === "^":
+                inputField.value = power(updateNumbers);
+                console.log(updateNumbers);
+                console.log(power(updateNumbers));
+                break;
+            case operator=== "C":
+                inputField.value = "";
+                numbers = [];
+                updateNumbers = [];
+                console.log(updateNumbers);
+                break;
+            default: 
+                console.log("error");
         }
-    });
-});
-
-const clearCalculation = document.querySelectorAll("#container .clearCalc");
-clearCalculation.forEach(button => {
-    button.addEventListener("click", function() {  
-        inputField.value = "";
-        numbers = [];
-        updateNumbers = [];
         
     });
 });
+
 const sum = function(numbers) {
-    return numbers.reduce(function(a, b){
-        console.log(a + b)
-        return a + b;
-    });
+    return numbers[1] + numbers[0];
+    // return numbers.reduce(function(a, b){
+    //     return a + b;
+    // });
 };
 
 const sub = function(numbers) {
-    return numbers.reduce((a, b) => a - b);
+    return numbers[1] - numbers[0];
+    // return numbers.reduce((a, b) => a - b);
 };
 
 const multiply = function(numbers) {
-    return numbers.reduce((a, b) => a * b, 1);
+    return numbers[1] * numbers[0];
+    //return numbers.reduce((a, b) => a * b, 1);
 };
 
 const div = function(numbers) {
-    return numbers.reduce((a, b) => a / b); 
+    return numbers[1] / numbers[0];
+    //return numbers.reduce((a, b) => a / b); 
 };
 
-// const power = function(numbers = []) {
-//     if (numbers.length < 2) {
-//         return 0;
-//     }
-//     return numbers[0] ** numbers[1];
-// };
+const power = function(numbers = []) {
+    if (numbers.length < 2) {
+        return 0;
+    }
+    return numbers[1] ** numbers[0];
+};
 
 
 
